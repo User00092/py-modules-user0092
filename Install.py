910638160b6eea8db5936d9190d8ea88af4f3b11
User0092 <103274860+User00092@users.noreply.github.com>
2022-08-21 12:46:54 -0400

    Add files via upload

---
+++ Install.py
@@ -0,0 +1,11 @@
+import os, sys
+current_folder = str(os.path.dirname(os.path.abspath(sys.argv[0]))).replace('\\', "/") + "/"
+appdata_folder = os.path.expandvars('%appdata%').replace(r"\Roaming", "").replace('\\', "/") + "/"
+python_folder = appdata_folder + "Local/Programs/Python/"
+for file in os.listdir(python_folder):
+    if "Python" in file:
+        python_folder = python_folder + file + "/"
+
+os.rename(current_folder + "userfiles.py", python_folder + "userfiles.py")
+
+print("Installed package")
