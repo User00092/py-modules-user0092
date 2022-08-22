b9de3142362968e32eaee5f8fcc35ea495243721
User0092 <103274860+User00092@users.noreply.github.com>
2022-08-22 18:03:32 -0400

    Update Install.py

--- Install.py
+++ Install.py
@@ -4,7 +4,7 @@ appdata_folder = os.path.expandvars('%appdata%').replace(r"\Roaming", "").replac
 python_folder = appdata_folder + "Local/Programs/Python/"
 for file in os.listdir(python_folder):
     if "Python" in file:
-        python_folder = python_folder + file + "/"
+        python_folder = python_folder + file + "/Lib/"
 
 os.rename(current_folder + "userfiles.py", python_folder + "userfiles.py")
 
