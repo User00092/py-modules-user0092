5b7c973bab93d2b1142693e2285e86c641b2d469
User0092 <103274860+User00092@users.noreply.github.com>
2022-09-13 18:20:26 -0400

    Updated install.py

--- Install.py
+++ Install.py
@@ -1,11 +1,17 @@
-import os, sys
+import os
+import sys
+import shutil
+
 current_folder = str(os.path.dirname(os.path.abspath(sys.argv[0]))).replace('\\', "/") + "/"
 appdata_folder = os.path.expandvars('%appdata%').replace(r"\Roaming", "").replace('\\', "/") + "/"
 python_folder = appdata_folder + "Local/Programs/Python/"
+
+
 for file in os.listdir(python_folder):
     if "Python" in file:
         python_folder = python_folder + file + "/Lib/"
 
-os.rename(current_folder + "userfiles.py", python_folder + "userfiles.py")
+ 
+shutil.move(current_folder + "userfiles.py", python_folder + "userfiles.py")
 
-print("Installed package")
+input("Installed package")
