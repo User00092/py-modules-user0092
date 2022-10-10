04998f083a00120ab77004d3d155d0d4200a961f
User0092 <103274860+User00092@users.noreply.github.com>
2022-10-09 22:51:10 -0400

    Updated usefiles.py
    Made code cleaner and easier to understand.

--- userfiles.py
+++ userfiles.py
@@ -1,267 +1,185 @@
 import inspect
 from inspect import currentframe, getframeinfo
+import os
 
-opened_file = []
-def read(file):
-    for data in opened_file:
-        opened_file.remove(data)
-    if (file.endswith(".userfiles")):
-        opened_file.append(file)
-    else:
-        raise Exception("Could not read invalid file type.")
-    
-def get_file():
-    if len(opened_file) == 1:
-        return opened_file[0]
-    else:
+
+class File:
+    opened_file = []
+
+
+class userfiles:
+    @staticmethod
+    def read(file: str):
+        if not os.path.exists(file):
+            frameinfo = getframeinfo(currentframe())
+            raise Exception(f"Error on line {frameinfo.lineno - 1}\n{file} does not exist.")
+
+        if not file.lower().endswith(".userfiles"):
+            frameinfo = getframeinfo(currentframe())
+            raise Exception(f"Error on line {frameinfo.lineno - 1}\nCannot read invalid file type\n{file}.")
+        
+        File.opened_file.clear()
+        File.opened_file.insert(0, file)
+
+    @staticmethod
+    def get_file():
+        if File.opened_file:
+            return File.opened_file[0]
+        
         return None
-
-def add_item(id_, item_):
-    try:
-        frameinfo = getframeinfo(currentframe())
     
-        file_ = get_file()
-        if not file_:
-            raise Exception(f"Traceback: Error on line {frameinfo.lineno} in the function 'add_item'.\nError: No file has been read.")
-        id_ = str(id_).lower()
-        new_string = ""
-        current_cats = get_catagories()
-        if f"[{id_}]" in current_cats:
-            for cat in current_cats:
-                if cat == f"[{id_}]":
-                    cat_data = get_category_items(id_)
-                    new_string += f"\ncategory:[{id_}]:data:\n"
-                    for index, line in enumerate(cat_data):
-                        line = str(line).replace("\n", "")
-                        new_string += f"{line}\n"
-                    new_string += f"{item_}\n"
-                else:
-                    cat_data = get_category_items(cat.replace("[","").replace("]",""))
-                    new_string += f"\ncategory:{cat}:data:\n"
-                    for index, line in enumerate(cat_data):
-                        new_string += f"{line}\n"
-            with open(file_, 'w') as f:
-                f.write(new_string)        
-            
-            
-        else:   
-            raise Exception(f"{id_} not found.")
-        
-        
-    except Exception as error:
-        raise Exception(error)
+    @staticmethod
+    def add_item(cat, item, value):
+        categories = userfiles.get_categories()
+        if cat not in categories:
+            frameinfo = getframeinfo(currentframe())
+            raise Exception(f"Error on line {frameinfo.lineno - 1}\nCategory '{cat}' not available.")
 
-def remove_item(id_, item_):
-    try:
-        frameinfo = getframeinfo(currentframe())
-    
-        file_ = get_file()
-        if not file_:
-            raise Exception(f"Traceback: Error on line {frameinfo.lineno} in the function 'remove_item'.\nError: No file has been read.")
-        id_ = str(id_).lower()
-        new_string = ""
-        current_cats = get_catagories()
-        if f"[{id_}]" in current_cats:
-            for cat in current_cats:
-                if cat == f"[{id_}]":
-                    cat_data = get_category_items(id_)
-                    new_string += f"\ncategory:[{id_}]:data:\n"
-                    for index, line in enumerate(cat_data):
-                        line = str(line).replace("\n", "")
-                        if line == item_:
+        data = []
+        
+        for category in categories:
+            if category != cat:
+                data.append(f'category:[{category}]:data:')
+                data.append("\n".join(userfiles.get_category_items(category)))
+
+            else:
+                data.append(f'category:[{cat}]:data:')
+                category_data = userfiles.get_category_items(category)
+                data.append("\n".join(userfiles.get_category_items(category)))
+                data.append(f"{item} = {value}")
+        
+        new_data = "\n".join(data)
+        with open(userfiles.get_file(), 'w') as file:
+            file.write(new_data)
+            file.close()
+            
+    @staticmethod
+    def remove_item(cat, item):
+        categories = userfiles.get_categories()
+        if cat not in categories:
+            frameinfo = getframeinfo(currentframe())
+            raise Exception(f"Error on line {frameinfo.lineno - 1}\nCategory '{cat}' not available.")
+
+        data = []
+        
+        for category in categories:
+            if category != cat:
+                data.append(f'category:[{category}]:data:')
+                data.append("\n".join(userfiles.get_category_items(category)))
+            
+            else:
+                data.append(f'category:[{category}]:data:')
+                cat_data = userfiles.get_category_items(category)
+                for data_ in cat_data:
+                    try:
+                        if data_.split('=')[0].strip() == item:
                             pass
-                        
-                        elif "=" in line:
-                            try:
-                                log_ = line.split("=")
-                                if log_[0].replace(" ", "") == item_:
-                                    pass
-                                else:
-                                    new_string += f"{line}\n"
-                            except:
-                                pass
                         else:
-                            new_string += f"{line}\n"
-                        
-                else:
-                    cat_data = get_category_items(cat.replace("[","").replace("]",""))
-                    new_string += f"\ncategory:{cat}:data:\n"
-                    for index, line in enumerate(cat_data):
-                        new_string += f"{line}\n"
-            
-            with open(file_, 'w') as f:
-                f.write(new_string)        
-            
-        else:   
-            raise Exception(f"{id_} not found.")
+                            data.append(f'{data_}')
+                    except:
+                        pass
+                
+        new_data = "\n".join(data)
         
-    except Exception as error:
-        raise Exception(error)
-
-def edit_value(id_, item_, to_value_):
-    frameinfo = getframeinfo(currentframe())
+        with open(userfiles.get_file(), 'w') as file:
+            file.write(new_data)
+            file.close()
+                    
     
-    file_ = get_file()
-    if not file_:
-        raise Exception(f"Traceback: Error on line {frameinfo.lineno} in the function 'edit_value'.\nError: No file has been read.")
-        
-    try:
-        new_string = ""
-        current_cats = get_catagories()
-        if f"[{id_}]" in current_cats:
-            for cat in current_cats:
-                if cat == f"[{id_}]":
-                    cat_data = get_category_items(id_)
-                    new_string += f"\ncategory:[{id_}]:data:\n"
-                    for index, line in enumerate(cat_data):
-                        line = str(line).replace("\n", "")
-                        if not "=" in line:
-                            if item_ == line:
-                                new_string += f"{to_value_}\n"
-                                
-                            else:
-                                new_string += f"{line}\n"
-                                
-                        else:
-                            log = line.split("=")
-                            if log[0][:-1] == item_ or log[0] == item_:
-                                new_string += f"{item_} = {to_value_}\n"
-                            else:
-                                new_string += f"{line}\n"
-                else:
-                    cat_data = get_category_items(cat.replace("[","").replace("]",""))
-                    new_string += f"\ncategory:{cat}:data:\n"
-                    for index, line in enumerate(cat_data):
-                        new_string += f"{line}\n"
-            
-            with open(file_, 'w') as f:
-                f.write(new_string)
-        
-        else:   
-            raise Exception(f"{id_} not found.")
-        
-    except Exception as error:
-        raise Exception(error)
+    @staticmethod
+    def edit_item(cat, item, value):
+        categories = userfiles.get_categories()
+        if cat not in categories:
+            frameinfo = getframeinfo(currentframe())
+            raise Exception(f"Error on line {frameinfo.lineno - 1}\nCategory '{cat}' not available.")
 
-def get_catagories():
-    cats = []
-    try:
-        frameinfo = getframeinfo(currentframe())
-        file_ = get_file()
+        data = []
         
-        if not file_:
-            raise Exception(f"Traceback: Error on line {frameinfo.lineno} in the function 'get_catagories'.\nError: No file has been read.")
+        for category in categories:
+            if category != cat:
+                data.append(f'category:[{category}]:data:')
+                data.append("\n".join(userfiles.get_category_items(category)))
+            
+            else:
+                data.append(f'category:[{category}]:data:')
+                cat_data = userfiles.get_category_items(category)
+                for data_ in cat_data:
+                    try:
+                        if data_.split('=')[0].strip() == item:
+                            data.append(f'{item} = {value}')
+                        else:
+                            data.append(f'{data_}')
+                    except:
+                        pass
+                
+        new_data = "\n".join(data)
         
-        with open(file_, 'r') as f:
-            file_data_ = f.read()
-            f.close()
-        
-        file_data_split_ = file_data_.split("category:")
-        for index, data_ in enumerate(file_data_split_):
-            log_ = data_.split(":data:")
-            cats.append(log_[0].lower())
-        
-        cats.remove(cats[0])
+        with open(userfiles.get_file(), 'w') as file:
+            file.write(new_data)
+            file.close()
+    
+    @staticmethod
+    def get_categories():
+        cats = []
+        file_data = open(userfiles.get_file(), 'r').read()
+        for index, data in enumerate(file_data.split('category:')):
+            log = data.split(":data:")
+            if not log[0] == "":
+                cats.append(log[0].replace("[", "").replace("]", ""))
+                
         return cats
     
-    except Exception as error:
-        raise Exception(error)
+    @staticmethod
+    def get_category_items(cat):
+        data = []
+        categories = userfiles.get_categories()
+        if cat not in categories:
+            frameinfo = getframeinfo(currentframe())
+            raise Exception(f"Error on line {frameinfo.lineno - 1}\nCategory '{cat}' not available.")
 
-def get_category_items(id_):
-    try:
-        data_ = []
-        id_ = id_.lower()
-        frameinfo = getframeinfo(currentframe())
-        file_ = get_file()
+        file_data = open(userfiles.get_file(), 'r').read()
+        log = file_data.split(":data:")
+        for line in log[1].splitlines():
+            if line and line != "":
+                data.append(line)
         
-        if not file_:
-            raise Exception(f"Traceback: Error on line {frameinfo.lineno} in the function 'get_catagories'.\nError: No file has been read.")
-        
-        with open(file_, 'r') as f:
-            file_data_ = f.read()
-            f.close()
-        
-        file_data_split_ = file_data_.split("category:")
-        
-        for index, line in enumerate(file_data_split_):
-            log_ = line.split(":data:")
-            if log_[0] == f"[{id_}]":
-                data_ = log_[1].splitlines()
-
-            for i in data_:
-                if i == "":
-                    data_.remove(i)
-        
-        return data_
+        return data
     
-    except Exception as error:
-       raise Exception(error)
-    
-def get(id_, item_):
-    try:
-        id_ = id_.lower()
-        frameinfo = getframeinfo(currentframe())
-        file_ = get_file()
-        if not file_:
-            raise Exception(f"Traceback: Error on line {frameinfo.lineno} in the function 'get'.\nError: No file has been read.")
-        
-        frameinfo = getframeinfo(currentframe())
-        file_catagories_ = get_catagories()
-        if str(f"[{id_}]") in file_catagories_:
-            pass
-        else:
-            raise Exception(f"Traceback: Error on line {frameinfo.lineno} in the function 'get'.\nError: No ID named \"{id_}\".")
-            
-        cat_data = get_category_items(id_)
-        
-        for i in cat_data:
-            log = i.split("=")
-            if log[0] == item_ or log[0][:-1] == item_:
-                if log[1] == " " or log[1] == "":
-                    return ""
-                else:
-                    return log[1]
-        
-        raise Exception(f"Traceback: Error on line {frameinfo.lineno} in the function 'get'.\nError: Data not available.")
-    
-    except Exception as error:
-        raise Exception(error)
-
-def add_category(name_):
-    try:
-        frameinfo = getframeinfo(currentframe())
-        file_ = get_file()
-        if not file_:
-            raise Exception(f"Traceback: Error on line {frameinfo.lineno} in the function 'add_category'.\nError: No file has been read.")
-        
-        with open(file_, 'r') as f:
-            data = f.read()
-            
-        if not f"category:[{name_}]:data:" in data.splitlines():   
-            if len(data.splitlines()) == 0:
-                with open(file_, 'w') as f:
-                    f.write(f"category:[{name_}]:data:")
-            else:
-                with open(file_, 'a') as f:
-                    f.write(f"\n\ncategory:[{name_}]:data:")
+    @staticmethod
+    def get(cat, item):
+        category_data = userfiles.get_category_items(cat)
+        for data in category_data:
+            try:
+                if data.split("=")[0].strip() == item:
+                    return data.split("=")[1].strip()
                 
-            return True
+            except:
+                pass
         
-        else:
-            return False
+        frameinfo = getframeinfo(currentframe())
+        raise Exception(f"Error on line {frameinfo.lineno - 7}\nCategory '[{cat}] {item}' is not available.")
+    
+    @staticmethod
+    def add_category(cat):
+        with open(userfiles.get_file(), 'a') as file:
+            file.write(f"\ncategory:[{str(cat)}]:data:\n")
+            file.close()
+    
+    def remove_category(cat):
+        categories = userfiles.get_categories()
+        if cat not in categories:
+            frameinfo = getframeinfo(currentframe())
+            raise Exception(f"Error on line {frameinfo.lineno - 1}\nCategory '{cat}' not available.")
+
+        data = []
         
-    except Exception as error:
-        raise Exception(error)
+        for category in categories:
+            if category != cat:
+                data.append(f'category:[{category}]:data:')
+                data.append("\n".join(userfiles.get_category_items(category)))
 
-
-class userfiles():
-    read = read
-    add_category = add_category
-    get = get
-    get_category_items = get_category_items
-    get_catagories = get_catagories
-    remove_item = remove_item
-    add_item = add_item
-    get_file = get_file
-    edit_value = edit_value
-
+        new_data = "\n".join(data)
+        
+        with open(userfiles.get_file(), 'w') as file:
+            file.write(new_data)
+            file.close()
